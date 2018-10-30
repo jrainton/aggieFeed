af5b270fb80edc6969063cc84fa2bdbb64951e6e
Jay Rainton <jayrainton@gmail.com>
2018-10-30 00:57:41 -0700

    added controller to component and added to main page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,12 +17,21 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+  .component('phoneList', {
+    templateUrl: 'views/phoneList.html',
+    controller: 'PhoneListController'
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the aggieFeedApp
  */
-angular.module('aggieFeedApp')
-  .controller('MainCtrl', function () {
+
+var app = angular.module('aggieFeedApp');
+
+app.controller('MainCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
   });
+
+app.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
